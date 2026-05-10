6661532dbac200f0e0803d46d2a5cd244e4d9081
Basanta Bomjan <123456789+odin@users.noreply.github.com>
2026-05-10 22:31:10 +0545

    chore: move sensitive config values to environment

--- includes/config.php
+++ includes/config.php
@@ -23,14 +23,13 @@ define('DB_NAME', 'vehicle_rental');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-// Use Gmail app password here, not normal Gmail password.
 define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
-define('MAIL_USERNAME', 'basantabomjan095@gmail.com');
-define('MAIL_PASSWORD', 'idazngydmgjcltwm');
-define('MAIL_FROM', 'basantabomjan095@gmail.com');
+define('MAIL_USERNAME', env_value('MAIL_USERNAME', ''));
+define('MAIL_PASSWORD', env_value('MAIL_PASSWORD', ''));
+define('MAIL_FROM', env_value('MAIL_FROM', MAIL_USERNAME));
 
-define('JWT_SECRET', 'change-this-demo-secret');
+define('JWT_SECRET', env_value('JWT_SECRET', 'change-this-demo-secret'));
 define('JWT_EXPIRE_SECONDS', 900);
 define('REFRESH_EXPIRE_DAYS', 7);
 
